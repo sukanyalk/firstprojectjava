36f9b880e2c22dd99f14e186ee768cbbeb5c8b63
manuh <manuh@LAPTOP-EU21NM8B>
2023-01-12 12:54:58 -0500

    I am uploading all files of my project for the first time

---
+++ src/Training/Hello.java
@@ -0,0 +1,16 @@
+package Training;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		
+System.out.println("Welcome to the world of DevOps ");
+System.out.println("Hi this is Sukanya");
+
+		System.out.println("Thank you");
+System.out.println(20+40);
+		
+	}
+
+}
---
+++ src/Training/multiplication.java
@@ -0,0 +1,12 @@
+package Training;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(5*4);
+
+		System.out.println("hello,this is nice");
+	}
+
+}
